2ac81cf4f128b4a100fbed5475f860f85492223b
Serpent03 <dumbpilot03@gmail.com>
2025-07-20 22:23:25 +0530

    xyz

--- main.cpp
+++ main.cpp
@@ -27,24 +27,24 @@ int main() {
 
   printf("[ %f, %f, %f ]\n", sx, sy, sz);
 
-  bool path = ss.solve(20);
-  if (!path) return 0;
+  // bool path = ss.solve(20);
+  // if (!path) return 0;
 
-  // ss.simplifySolution();
-  auto pts = ss.getSolutionPath();
+  // // ss.simplifySolution();
+  // auto pts = ss.getSolutionPath();
 
-  // @TODO: iterate  and see all the generated points
-  // !path
-  for (int i = 0; i < pts.getStateCount(); i++) {
-    float x, y, z;
-    auto *cstate = pts.getState(i)->as<ob::RealVectorStateSpace::StateType>();
-    x = cstate->values[0];
-    y = cstate->values[1];
-    z = cstate->values[2];
-    wp.otree.updateNode(POINT(x,y,z), false);
-    printf("[ %f %f %f ]\n", x,y,z);
+  // // @TODO: iterate  and see all the generated points
+  // // !path
+  // for (int i = 0; i < pts.getStateCount(); i++) {
+  //   float x, y, z;
+  //   auto *cstate = pts.getState(i)->as<ob::RealVectorStateSpace::StateType>();
+  //   x = cstate->values[0];
+  //   y = cstate->values[1];
+  //   z = cstate->values[2];
+  //   wp.otree.updateNode(POINT(x,y,z), false);
+  //   printf("[ %f %f %f ]\n", x,y,z);
     
-  }
+  // }
   
-  wp.otree.writeBinary("path.bt");
+  // wp.otree.writeBinary("path.bt");
 }
